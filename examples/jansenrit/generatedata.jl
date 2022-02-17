549d15a6256d65a6a1862f222df5b93c10736d5d
fmeulen <f.h.vandermeulen@tudelft.nl>
2022-02-17 12:35:18 +0100

    code import

---
+++ examples/jansenrit/generatedata.jl
@@ -0,0 +1,107 @@
+#Random.seed!(5)
+
+
+model= [:jr, :jr3][1]
+
+if model == :jr
+  θ0 =[3.25, 100.0, 22.0, 50.0, 135.0, 0.8, 0.25, 5.0, 6.0, 0.56, 200.0, 6.0]  # except for μy as in Buckwar/Tamborrino/Tubikanec#
+  #θ0 =[3.25, 100.0, 22.0, 50.0, 185.0, 0.8, 0.25, 5.0, 6.0, 0.56, 200.0, 6.0]  # this gives bimodality
+ # θ0 =[3.25, 100.0, 22.0, 50.0, 530.0, 0.8, 0.25, 5.0, 6.0, 0.56, 200.0, 5.0]  # also try this one
+  ℙ0 = JansenRitDiffusion(θ0...)
+  @show properties(ℙ0)
+  AuxType = JansenRitDiffusionAux
+end
+if model == :jr3
+  θ0 =[3.25, 100.0, 22.0, 50.0, 135.0, 0.8, 0.25, 5.0, 6.0, 0.56, 200.0, 0.01, 200.0, 1.0]  # except for μy as in Buckwar/Tamborrino/Tubikanec#
+  ℙ0 = JansenRitDiffusion3(θ0...)
+  AuxType = JansenRitDiffusionAux3
+end
+
+#------  set observations
+L = @SMatrix [0.0 1.0 -1.0 0.0 0.0 0.0]
+m,  = size(L)
+Σdiagel = 1e-7 # oorspr 1e-9
+Σ = SMatrix{m,m}(Σdiagel*I)
+
+#---- generate test data
+T = 2.0 #1.0
+x00 = @SVector zeros(6)
+W = sample((-1.0):0.0001:T, wienertype(ℙ0))                        #  sample(tt, Wiener{ℝ{1}}())
+Xf_prelim = Bridge.solve(Bridge.Euler(), x00, W, ℙ0)
+# drop initial nonstationary behaviour
+Xf = SamplePath(Xf_prelim.tt[10001:end], Xf_prelim.yy[10001:end])
+x0 = Xf.yy[1]
+dt = Xf.tt[2]-Xf.tt[1]
+
+skipobs = 400# I took 400  all the time
+obstimes =  Xf.tt[1:skipobs:end]
+obsvals = map(x -> L*x, Xf.yy[1:skipobs:end])
+pF = plot_all(ℙ0,  Xf, obstimes, obsvals)
+savefig(joinpath(outdir, "forwardsimulated.png"))
+
+#------- process observations, assuming x0 known
+obs = [Observation(obstimes[1],  x0,  SMatrix{6,6}(1.0I), SMatrix{6,6}(Σdiagel*I))]
+for i in 2:length(obstimes)
+  push!(obs, Observation(obstimes[i], obsvals[i], L, Σ));
+end
+
+@show ℙ0
+
+#----------- obs and obsvals are input to mcmc algorithm
+
+# -- now obs with staionary prior on x0
+obs = [Observation(-1.0,  x00,  SMatrix{6,6}(1.0I), SMatrix{6,6}(Σdiagel*I))]
+for i in 1:length(obstimes)
+  push!(obs, Observation(obstimes[i], obsvals[i], L, Σ));
+end
+pushfirst!(obsvals, SA[0.0])
+Xf = Xf_prelim
+x0 = x00
+
+
+# remainder is checking 
+S = DE(Vern7())
+
+timegrids = set_timegrids(obs, 0.0005)
+B = BackwardFilter(S, ℙ0, AuxType, obs, obsvals, timegrids) ;
+Z = Innovations(timegrids, ℙ0);
+
+# check
+XX, ll = forwardguide(B, ℙ0)(x0, Z);
+pG = plot_all(ℙ0, timegrids, XX)
+l = @layout [a;b]
+plot(pF, pG, layout=l)
+savefig(joinpath(outdir,"forward_guidedinitial_separate.png"))
+
+plot_all(ℙ0,Xf,  obstimes, obsvals, timegrids, XX)
+savefig(joinpath(outdir,"forward_guidedinitial_overlaid.png"))
+
+
+deviations = [obsvals[i] - L * XX[i-1][end]  for i in 2:length(obs)]
+plot(obstimes[2:end], first.(deviations))
+savefig(joinpath(outdir,"deviations_guidedinitial.png"))
+
+
+TEST=false 
+if TEST
+
+# test
+S = Vern7direct();
+@time BackwardFilter(S, ℙ, AuxType, obs, obsvals, timegrids);
+
+
+S = DE(Vern7())
+@time  BackwardFilter(S, ℙ, AuxType, obs,obsvals, timegrids);
+# S = RK4()
+# @btime  BackwardFilter(S, ℙ, AuxType, obs, timegrids);
+@time forwardguide(x0, ℙ, Z, B);
+
+# using Profile
+# using ProfileView
+# Profile.init()
+# Profile.clear()
+# S = DE(Vern7())#S = Vern7direct();
+# @profile  BackwardFilter(S, ℙ, AuxType, obs, obsvals, timegrids);
+# @profile parupdate!(B, XX, movetarget, obs, obsvals, S, AuxType, timegrids; verbose=verbose)(x0, ℙ, Z, ll);# θ and XX may get overwritten
+# ProfileView.view()
+end
---
+++ examples/jansenrit/jansen_rit_example.jl
@@ -0,0 +1,230 @@
+wdir = @__DIR__
+cd(wdir)
+outdir= joinpath(wdir, "out")
+
+using Revise
+using GuidingDiffusion
+using Plots
+
+# using Bridge, StaticArrays, Distributions
+# #using Test, Statistics, 
+# using Random, LinearAlgebra
+# using Bridge.Models
+# #using DelimitedFiles
+# #using DataFrames
+# # using CSV
+# #using ForwardDiff
+# using DifferentialEquations
+# using Setfield
+# using Plots
+# using ConstructionBase
+# using Interpolations
+# using IterTools
+# using ProfileView
+# #using RCall
+# #using SparseArrays
+# #using Parameters
+
+#import Bridge: R3, IndexedTime, llikelihood, kernelr3, constdiff, Euler, solve, solve!
+import Bridge: constdiff
+
+sk = 0 # skipped in evaluating loglikelihood
+
+include("jansenrit.jl")
+include("jansenrit3.jl")
+
+include("plotting.jl")
+
+################################  TESTING  ################################################
+
+S = DE(Vern7())
+
+include("generatedata.jl")
+
+timegrids = set_timegrids(obs, 0.0005)
+
+
+iterations = 2_000  #5_00
+skip_it = 200
+subsamples = 0:skip_it:iterations # for saving paths
+
+# define priors
+priorC = Exponential(150.0)
+priorσ = Uniform(0.0, 1_000.0)
+priorα1 = Uniform(0.0,1.0)
+
+# define parameter moves
+moveC = ParMove([:C], parameterkernel((short=[1.0], long=[5.0]); s=0.0), priorC, false)
+
+moveσ = ParMove([:σ], parameterkernel((short=[3.0], long=[10.0]); s=0.0), priorσ, true)
+
+moveCᵒ = ParMove([:C], parameterkernel((short=[40.0], long=[100.0])), priorC, false)
+
+moveCσ = ParMove([:C, :σ], parameterkernel((short=[2.0, 10.0], long=[10.0, 10.0]); s=0.0), product_distribution([priorC, priorσ]), true)
+
+moveCσα1 = ParMove([:C, :σ, :α1], parameterkernel((short=[2.0, 10.0, 0.01], long=[10.0, 10.0, 0.1]); s=0.0), product_distribution([priorC, priorσ, priorα1]), true)
+
+moveCα1 = ParMove([:C, :α2], parameterkernel((short=[2.0,  0.01], long=[10.0,  0.1]); s=0.3), product_distribution([priorC,  priorα1]), false)
+# a small program
+
+# settings
+verbose = true # if true, surpress output written to console
+
+
+θinit = 100.0
+ESTσ = true
+
+
+
+if ESTσ
+  θ = (C=copy(θinit), σ = 2.0)
+  movetarget = moveCσ
+  allparnames = [:C, :σ]
+else
+  θ = (; C = copy(θinit) ) # initial value for parameter
+  movetarget = moveC
+  allparnames = [:C]
+end
+ℙ = setproperties(ℙ0, θ)
+
+θ = (C=copy(θinit), α1=0.5)
+movetarget = moveCα1
+allparnames = [:C,  :α2]
+ℙ = setproperties(ℙ0, θ)
+
+𝒯 = 40.0 # temperature
+ℙe = setproperties(ℙ0, C=copy(θinit),  σ = 𝒯)
+allparnamese = [:C]
+move_exploring = moveCᵒ
+
+
+
+# pcn pars 
+ρ = 0.95
+ρe = 0.95
+
+# initialisation of target chain 
+B = BackwardFilter(S, ℙ, AuxType, obs, obsvals, timegrids);
+Z = Innovations(timegrids, ℙ);
+Zbuffer = deepcopy(Z)
+Zᵒ = deepcopy(Z)
+ρs = fill(ρ, length(timegrids))
+XX, ll = forwardguide(B, ℙ)(x0, Z);
+
+
+# initialisation of exploring chain 
+Be = BackwardFilter(S, ℙe, AuxType, obs, obsvals, timegrids);
+Ze = Innovations(timegrids, ℙe);# deepcopy(Z);
+Zeᵒ = deepcopy(Ze)
+ρse = fill(ρe, length(timegrids))
+XXe, lle = forwardguide(Be, ℙe)(x0, Ze);
+
+
+
+θsave = [copy(getpar(allparnames, ℙ))]
+XXsave = [copy(XX)]
+llsave = [ll]
+
+XXesave = [copy(XXe)]
+
+
+
+accinnov = 0
+accpar = 0
+accinnove = 0
+accpare = 0
+accmove = 0
+
+
+exploring = [State(x0, copy(Ze), getpar(allparnamese,ℙe), copy(lle))]
+
+for i in 1:iterations
+  (i % 500 == 0) && println(i)
+  
+  # update exploring chain
+  if i==1  
+  lle, Be, ℙe, accpare_ = parupdate!(Be, XXe, move_exploring, obs, obsvals, S, AuxType, timegrids; verbose=verbose)(x0, ℙe, Ze, lle);# θe and XXe may get overwritten
+  lle, accinnove_ = pcnupdate!(Be, ℙe, XXe, Zbuffer, Zeᵒ, ρse)(x0, Ze, lle); # Z and XX may get overwritten
+  push!(exploring, State(x0, copy(Ze), getpar(allparnamese, ℙe), copy(lle)))   # collection of samples from exploring chain
+  end
+
+  # update target chain
+  smallworld = rand() > 0#0.33
+  if smallworld
+    ll, B, ℙ, accpar_ = parupdate!(B, XX, movetarget, obs, obsvals, S, AuxType, timegrids; verbose=verbose)(x0, ℙ, Z, ll);# θ and XX may get overwritten
+    accmove_ =0
+  else
+    w = sample(exploring)     # randomly choose from samples of exploring chain
+    ll, ℙ,  accmove_ = exploremoveσfixed!(B, Be, ℙe, move_exploring, XX, Zᵒ, w; verbose=verbose)(x0, ℙ, Z, ll) 
+    accpar_ = 0
+    #println(ℙ.C ==θ[1])
+  end  
+  ll, accinnov_ = pcnupdate!(B, ℙ, XX, Zbuffer, Zᵒ, ρs)(x0, Z, ll); # Z and XX may get overwritten
+
+  
+  # update acceptance counters
+#  accpar += accpar_; accpare += accpare_; accinnove += accinnove_; accinnov += accinnov_; accmove += accmove_
+  # saving iterates
+  push!(θsave, getpar(allparnames, ℙ))
+  push!(llsave, ll)
+  (i in subsamples) && push!(XXsave, copy(XX))
+  (i % 500 == 0) && push!(XXesave, XXe)
+  
+  adjust_PNCparamters!(ρs, ρ)
+end
+
+
+
+
+# final imputed path
+plot_all(ℙ, timegrids, XXsave[end])
+
+obstimes = getfield.(obs, :t)
+
+plot_all(ℙ, Xf, obstimes, obsvals, timegrids, XXsave[1])
+savefig(joinpath(outdir,"guidedpath_firstiteration.png"))
+plot_all(ℙ, Xf, obstimes, obsvals, timegrids, XXsave[end])
+savefig(joinpath(outdir,"guidedpath_finaliteration.png"))
+
+plot_all(ℙ, Xf, obstimes, obsvals, timegrids, XXesave[1])
+savefig(joinpath(outdir,"guidedpath_firstiteration_exploring.png"))
+plot_all(ℙ, Xf, obstimes, obsvals, timegrids, XXesave[end])
+savefig(joinpath(outdir,"guidedpath_finaliteration_exploring.png"))
+
+
+#
+println("Target chain: accept% innov ", 100*accinnov/iterations,"%")
+println("Target chain: accept% par ", 100*accpar/iterations,"%")
+println("Exploring chain: accept% innov ", 100*accinnove/iterations,"%")
+println("Exploring chain: accept% par ", 100*accpare/iterations,"%")
+
+println("accept% swap ", 100*accmove/iterations,"%")
+
+θesave = getindex.(getfield.(exploring,:θ),1)
+llesave = getfield.(exploring, :ll)
+
+h1 = histogram(getindex.(θsave,1),bins=35, label="target chain")
+h2 = histogram(getindex.(θesave,1),bins=35, label="exploring chain")
+plot(h1, h2, layout = @layout [a b])  
+
+p1 = plot(llsave, label="target",legend=:bottom)    
+plot!(p1,llesave, label="exploring")    
+savefig(joinpath(outdir,"logliks.png"))
+
+# traceplots
+pa = plot(getindex.(θsave,1), label="target", legend=:top)
+hline!(pa, [ℙ0.C], label="",color=:black)
+plot!(pa, getindex.(θesave,1), label="exploring")
+
+if ESTσ 
+  pb = plot(getindex.(θsave,2), label="target", legend=:top)
+  hline!(pb, [ℙ0.σ], label="",color=:black)
+  plot(pa, pb, layout = @layout [a; b])  
+end
+savefig(joinpath(outdir,"traceplots.png"))
+
+
+scatter(getindex.(θsave,1), getindex.(θsave,2))
+
+
+
